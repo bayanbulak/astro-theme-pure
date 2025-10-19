40b0ea2154a1dfb8788f1c64da576cc0b1dd0f0b
batu <ericzhangxxx@gmail.com>
2025-10-20 01:32:49 +0800

    desc

--- src/site.config.ts
+++ src/site.config.ts
@@ -7,7 +7,7 @@ export const theme: ThemeUserConfig = {
   /** Will be used in index page & copyright declaration */
   author: 'Bayanbulak',
   /** Description metadata for your website. Can be used in page metadata. */
-  description: 'Stay hungry, stay foolish',
+  description: 'En toute circonstance, fraîcheur de vivre.',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
   favicon: '/favicon/favicon.ico',
   /** Specify the default language for this site. */
